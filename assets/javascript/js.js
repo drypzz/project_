627c505b220c298ab00e1a51a51d888cb1419bf5
elseif <fdfjonfmd@gmail.com>
2022-05-19 11:31:25 -0300

    att css

---
+++ assets/javascript/js.js
@@ -0,0 +1,23 @@
+// active function
+
+document.querySelectorAll('.navbar-item a').forEach(isActive => {
+    isActive.addEventListener('click', function(){
+        document.querySelector('.navbar-menu').querySelector('.active').classList.remove('active')
+        isActive.classList.add('active')
+    })
+});
+
+// click navbar
+
+document.querySelector('#mobile-menu').addEventListener('click',
+    () => {
+        document.querySelector('#mobile-menu').classList.toggle('is-active')
+        document.querySelector('.navbar-menu').classList.toggle('active')
+    }
+);
+
+// util's
+
+getInsertDate = function(type){
+    return document.querySelector(type).innerText = new Date().getFullYear();
+};
