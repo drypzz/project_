5ebc6831b751273781fc1f8d1c6132ccd96f5a7b
elseif <fdfjonfmd@gmail.com>
2022-05-25 18:33:02 -0300

    att .html/.css

--- assets/javascript/global.js
+++ assets/javascript/global.js
@@ -1,1 +1,7 @@
+/* 
+--------------------------------
+    # Copyright © - 2° ano de T.I;
+    # Type: .js;
+--------------------------------
+*/
 document.querySelectorAll(".navbar-item a").forEach(e=>{e.addEventListener("click",function(){document.querySelector(".navbar-menu").querySelector(".active").classList.remove("active"),e.classList.add("active")})}),document.querySelector("#mobile-menu").addEventListener("click",()=>{document.querySelector("#mobile-menu").classList.toggle("is-active"),document.querySelector(".navbar-menu").classList.toggle("active")}),getInsertDate=function(e){return document.querySelector(e).innerText=(new Date).getFullYear()},getInsertDate("#date");
